e37ba1d9e1d0e2ad8be8b417beda9e927f33bb87
yashrewa <yashrewa00@gmail.com>
2024-01-11 17:11:48 +0530

    bug fix: zoom issue in zoomed in mode\

--- src/app/components/MainPage.tsx
+++ src/app/components/MainPage.tsx
@@ -58,16 +58,15 @@ const bottom = {
 
 
 
-
 export const MainPage = () => {
     return (
         <>
-            <motion.div variants={variants} initial="hidden" animate="show" className={`index-high h-full pl-10 flex items-start flex-col md:pt-64 justify-center text-white-50 font-poppins font-black text-5xl md:pl-60 md:text-8xl`}>
+            <motion.div variants={variants} initial="hidden" animate="show" className={`index-high h-full pl-10 flex items-start flex-col  lg:pt-64 justify-center text-white-50 font-poppins font-black text-5xl md:pl-60 md:text-8xl`}>
                 <motion.div variants={top} className="index-high tracking-widest pt-2"><span className="text-aqua-600" ></span>Hi</motion.div>
                 <motion.div variants={right} className="index-high tracking-widest pt-2">I&apos;m <span className="text-primary-purple">Yash</span></motion.div>
                 <motion.div variants={bottom} className="index-high tracking-widest text-3xl md:text-7xl pt-2">web <span className="text-aqua-600">d</span>eveloper</motion.div>
                 <motion.div variants={bottom} className="index-high font-medium text-sm md:text-xl  text-gray-850 pt-2">JavaScript <span className="text-aqua-450">/</span> MERN <span className="text-aqua-450">/</span> NextJs <span className="text-aqua-450">/</span> GraphQl <span className="text-aqua-450">/</span> Typescript <span className="text-aqua-450">/</span> Firebase <span className="text-aqua-450">/</span> Tailwind </motion.div>
-                <motion.button variants={top} className={`animate-bounce w-16 h-16 index-low absolute z-10 right-0 bottom-4 left-0 m-auto bg-gray-950 rounded-full p-3 flex items-center justify-center`}><Link href='/about'><AiOutlineDown className='text-4xl text-primary-purple' /></Link></motion.button>
+                <motion.button variants={top} className={`animate-bounce w-16 h-16 index-low absolute z-50 right-0 bottom-4 left-0 m-auto bg-gray-950 rounded-full p-3 flex items-center justify-center`}><Link href='/about'><AiOutlineDown className='text-4xl text-primary-purple' /></Link></motion.button>
             </motion.div>
 
         </>
