d2ead7cdd18586610f58dd734bf429ca2860b8fb
yashrewa <yashrewa00@gmail.com>
2024-02-25 21:00:21 +0530

    added some animation & changed background look

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
-// import {FaExternalLinkAlt} from 'react-icons/fa'
-// import {LuExternalLink} from 'react-icons/lu'
-// import {RiExternalLinkLine} from 'react-icons/ri'
 import { AiOutlineLink } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 // import Open from '../../Images/Open.svg'
 // import {BsGithub} from 'react-icons/bs'
 import { AiFillGithub } from 'react-icons/ai'
-// import Github from '../../Images/Socials/Github.svg'
 
 interface Props {
     img: string,
@@ -28,19 +23,7 @@ const variants = {
     }
 }
 
-const top = {
-    hidden: {
-        y: -100,
-        opacity: 0,
-    },
-    show: {
-        y: 0,
-        opacity: 1,
-        transition: {
-            duration: 1,
-        }
-    }
-}
+
 
 const right = {
     hidden: {
@@ -56,31 +39,18 @@ const right = {
         }
     }
 }
-const bottom = {
-    hidden: {
-        y: 700,
-        opacity: 1,
-    },
-    show: {
-        y: 0,
-        opacity: 1,
-        delay: 1,
-        transition: {
-            duration: 0.3,
-        }
-    }
-}
+
 export const Card = ({ img, heading, github, live, details }: Props) => {
     return (
-        <motion.div variants={right} initial='hidden' animate='show' className="index-high md:inline-block md:mr-10 bg-gray-950 md:hover:shadow-lg md:hover:scale-110 transition-all ease-in-out shadow-sm shadow-primary-purple md:hover:shadow-primary-purple h-60 w-40 md:w-64 md:h-80 flex flex-col items-center overflow-hidden rounded">
-            <div className="relative overflow-hidden">
+        <motion.div variants={right} initial='hidden' animate='show' className="index-high md:inline-block md:mr-10 bg-gray-950 md:hover:shadow-lg md:hover:scale-110 transition-all ease-in-out shadow-sm shadow-primary-purple md:hover:shadow-primary-purple h-72 w-40 md:w-64 md:h-80 flex flex-col justify-between items-center overflow-hidden rounded">
+            <div className="relative overflow-hidden ">
                 <span className="filter brightness-50"><Image src={img} className="card-image" height={200} width={260} alt="" /></span>
-                <div className="flex items-center justify-end absolute bottom-2 right-0 gap-x-1 pr-2">
-                    {github && <a target="_blank" rel="noreferrer" href={github}><span className="cursor-pointer text-3xl"><AiFillGithub className='text-xl' /></span></a >}
-                    {live && <a target="_blank" rel="noreferrer" href={live}><span className="cursor-pointer text-2xl"><AiOutlineLink className='text-8xl fill-primary-purple' /></span></a>}
+                <div className="flex items-center justify-end w-full absolute left-0 bottom-2 right-0 gap-x-1 pr-2">
+                    {github && <a target="_blank" rel="noreferrer" href={github}><span className="cursor-pointer text-3xl"><AiFillGithub className='text-3xl lg:text-4xl text-primary-purple' /></span></a >}
+                    {live && <a target="_blank" rel="noreferrer" href={live}><span className="cursor-pointer text-2xl "><AiOutlineLink className='text-3xl lg:text-4xl text-primary-purple' /></span></a>}
                 </div>
             </div>
-            <h4 className="text-aqua-600 font-semiBold pt-2 md:text-center md:mb-1">{heading}</h4>
+            <h4 className="text-aqua-600 font-semibold pt-2 md:text-center md:mb-1">{heading}</h4>
             <p className="pr-3 pl-3 font-extralight text-center text-xs pb-10">{details}</p>
         </motion.div>
     )
